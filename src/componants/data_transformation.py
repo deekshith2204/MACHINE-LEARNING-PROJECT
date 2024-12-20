ebb8bd3aef6a4797180a168d74c05e198398e763
deekshith2204 <deekshiththadvai2204@gmail.com>
2024-12-20 17:32:26 +0530

    transformation done sucessfully

--- src/componants/data_transformation.py
+++ src/componants/data_transformation.py
@@ -32,14 +32,14 @@ class DataTransformation:
             num_pipeline=Pipeline(
                 steps=[
                     ("imputer",SimpleImputer(strategy="median")),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
                 ]
             )
             cat_pipline=Pipeline(
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("onehotencoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
 
                 ]
             )
@@ -75,7 +75,7 @@ class DataTransformation:
             target_feature_test_df=test_df[target_column]
 
             input_feature_train_arr=processing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=processing_obj.fit_transform(input_feature_test_df)
+            input_feature_test_arr=processing_obj.transform(input_feature_test_df)
 
             train_arr=np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
