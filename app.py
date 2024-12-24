da096c88a1b538597a49c0942fe5563ef7b3f94c
deekshith2204 <deekshiththadvai2204@gmail.com>
2024-12-24 21:38:50 +0530

    modefied server

--- app.py
+++ app.py
@@ -5,14 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipline.predict_pipline import customdata,predictpipeline
 
-application=Flask(__name__)
-app=application
-@app.route('/')
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
@@ -37,4 +32,4 @@ def predict_datapoint():
 
 
 if __name__ =="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(debug=True)
